4e6232e66267c62a5b83a4de76e82a083f6898ce
Luke Gibbins <bg85lk@student.sunderland.ac.uk>
2018-09-18 19:12:19 +0100

    Completed module 8

---
+++ Pages/Greeting.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ChillAndGrill.Pages
+{
+    public class GreeterModel : PageModel
+    {
+        private IGreeter _greeter;
+        public string CurrentGreeting { get; set; }
+
+        public GreeterModel(IGreeter greeter)
+        {
+            _greeter = greeter;
+            //Could've had restaurant data in bere
+        }
+
+        public void OnGet(string name)
+        {
+            CurrentGreeting = $"{name}: {_greeter.GetMessageOfTheDay()}"; //String interpolation
+            //Coudl've return restaurant data on a GET --> an alternative to MVC
+        }
+
+        //public void OnPost()...would be here
+    }
+}
---
+++ ViewComponents/GreeterViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChillAndGrill.ViewComponents
+{
+    public class GreeterViewComponent : ViewComponent
+    {
+        //This class is solely used for the greeter message from the service. An alternative to creating a 'ViewModel'  
+        //Remember when referencing a service or interface it HAS to be initialised in the ctor
+        private IGreeter _greeter;
+
+        public GreeterViewComponent(IGreeter greeter)
+        {
+            _greeter = greeter;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var model = _greeter.GetMessageOfTheDay();
+            return View("Default", model); //Only requires the default if the model is of type:String
+        }
+    }
+}
